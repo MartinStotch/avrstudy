be62a47dd1a39513057d306c0eb7c9f9b4f00481
Martin Stotch <stotchmartin@gmail.com>
2020-02-20 14:02:29 +0300

    Добавлен заголовочник для символов ASCII 0-127

---
+++ my_ascii_chars.h
@@ -0,0 +1,132 @@
+// Control Characters
+#define ASCII_NUL                       0x00    // Null
+#define ASCII_SOH                       0x01    // Start of Heading
+#define ASCII_STX                       0x02    // Start of Text
+#define ASCII_EXT                       0x03    // End of Text
+#define ASCII_EOT                       0x04    // Endo of Trasmission
+#define ASCII_ENQ                       0x05    // Enquiry
+#define ASCII_ACK                       0x06    // Acknowledgement
+#define ASCII_BEL                       0x07    // Bell
+#define ASCII_BS                        0x08    // Backspace  
+#define ASCII_HT                        0x09    // Horisontal Tab
+#define ASCII_LF                        0x0A    // Line Feed
+#define ASCII_VT                        0x0B    // Vertical Tab
+#define ASCII_FF                        0x0C    // Form Feed
+#define ASCII_CR                        0x0D    // Carriage Return
+#define ASCII_SO                        0x0E    // Shift Out
+#define ASCII_SI                        0x0F    // Shift In
+#define ASCII_DLE                       0x10    // Data Link Escape
+#define ASCII_DC1                       0x11    // Device Control 1
+#define ASCII_DC2                       0x12    // Device Control 2
+#define ASCII_DC3                       0x13    // Device Control 3
+#define ASCII_DC4                       0x14    // Device Control 4
+#define ASCII_NAK                       0x15    // Negative Acknowledgement
+#define ASCII_SYN                       0x16    // Synchronous Idle
+#define ASCII_ETB                       0x17    // End of Transmission Block
+#define ASCII_CAN                       0x18    // Cancel
+#define ASCII_EM                        0x19    // End of Medium
+#define ASCII_SUB                       0x1A    // Substitute
+#define ASCII_ESC                       0x1B    // Escape
+#define ASCII_FS                        0x1C    // File Separator
+#define ASCII_GS                        0x1D    // Group Separator
+#define ASCII_RS                        0x1E    // Record Separator
+#define ASCII_US                        0x1F    // Unit Separator
+
+#define ASCII_DEL                       0x7F    // Delete
+
+// Printable Characters
+#define ASCII_SPACE                     0x20    //
+#define ASCII_EXCLAMATION_SIGN          0x21    // !            
+#define ASCII_QUOTATION_MARK            0x22    // "
+#define ASCII_HASH                      0x23    // #
+#define ASCII_DOLLAR_SIGN               0x24    // $
+#define ASCII_PERCENT_SIGN              0x25    // %
+#define ASCII_AMPERSAND                 0x26    // &
+#define ASCII_APOSTROPHE                0x27    // '
+#define ASCII_LEFT_ROUND_BRACKET        0x28    // (
+#define ASCII_RIGHT_ROUND_BRACKET       0x29    // )
+#define ASCII_ASTERISK                  0x2A    // *
+#define ASCII_PLUS_SIGN                 0x2B    // +
+#define ASCII_COMMA                     0x2C    // ,
+#define ASCII_HYPHEN_MINUS              0x2D    // -
+#define ASCII_PERIOD                    0x2E    // .
+#define ASCII_SLASH                     0x2F    // /
+#define ASCII_ZERO                      0x30    // 0
+#define ASCII_ONE                       0x31    // 1
+#define ASCII_TWO                       0x32    // 2
+#define ASCII_THREE                     0x33    // 3
+#define ASCII_FOUR                      0x34    // 4
+#define ASCII_FIVE                      0x35    // 5
+#define ASCII_SIX                       0x36    // 6
+#define ASCII_SEVEN                     0x37    // 7
+#define ASCII_EIGHT                     0x38    // 8
+#define ASCII_NINE                      0x39    // 9
+#define ASCII_COLON                     0x3A    // :
+#define ASCII_SEMICOLON                 0x3B    // ;
+#define ASCII_LESS_THAN_SIGN            0x3C    // <
+#define ASCII_EQUALS_SIGN               0x3D    // =
+#define ASCII_GREATER_THAN_SIGN         0x3E    // >
+#define ASCII_QUESTION_MARK             0x3F    // ?
+#define ASCII_AT_SIGN                   0x40    // @
+#define ASCII_BIG_LETTER_A              0x41    // A
+#define ASCII_BIG_LETTER_B              0x42    // B
+#define ASCII_BIG_LETTER_C              0x43    // C
+#define ASCII_BIG_LETTER_D              0x44    // D
+#define ASCII_BIG_LETTER_E              0x45    // E
+#define ASCII_BIG_LETTER_F              0x46    // F
+#define ASCII_BIG_LETTER_G              0x47    // G
+#define ASCII_BIG_LETTER_H              0x48    // H
+#define ASCII_BIG_LETTER_I              0x49    // I
+#define ASCII_BIG_LETTER_J              0x4A    // J
+#define ASCII_BIG_LETTER_K              0x4B    // K
+#define ASCII_BIG_LETTER_L              0x4C    // L
+#define ASCII_BIG_LETTER_M              0x4D    // M
+#define ASCII_BIG_LETTER_N              0x4E    // N
+#define ASCII_BIG_LETTER_O              0x4F    // O
+#define ASCII_BIG_LETTER_P              0x50    // P
+#define ASCII_BIG_LETTER_Q              0x51    // Q
+#define ASCII_BIG_LETTER_R              0x52    // R
+#define ASCII_BIG_LETTER_S              0x53    // S
+#define ASCII_BIG_LETTER_T              0x54    // T
+#define ASCII_BIG_LETTER_U              0x55    // U
+#define ASCII_BIG_LETTER_V              0x56    // V
+#define ASCII_BIG_LETTER_W              0x57    // W
+#define ASCII_BIG_LETTER_X              0x58    // X
+#define ASCII_BIG_LETTER_Y              0x59    // Y
+#define ASCII_BIG_LETTER_Z              0x5A    // Z
+#define ASCII_LEFT_SQAURE_BRACKET       0x5B    // [
+#define ASCII_BACKSLASH                 0x5C    // \ 
+#define ASCII_RIGHT_SQAURE_BRACKET      0x5D    // ]
+#define ASCII_CARET                     0x5E    // ^
+#define ASCII_UNDERSCORE                0x5F    // _
+#define ASCII_GRAVE_ACCENT              0x60    // `
+#define ASCII_SMALL_LETTER_A            0x61    // a
+#define ASCII_SMALL_LETTER_B            0x62    // b
+#define ASCII_SMALL_LETTER_C            0x63    // c
+#define ASCII_SMALL_LETTER_D            0x64    // d
+#define ASCII_SMALL_LETTER_E            0x65    // e
+#define ASCII_SMALL_LETTER_F            0x66    // f
+#define ASCII_SMALL_LETTER_G            0x67    // g
+#define ASCII_SMALL_LETTER_H            0x68    // h
+#define ASCII_SMALL_LETTER_I            0x69    // i
+#define ASCII_SMALL_LETTER_J            0x6A    // j
+#define ASCII_SMALL_LETTER_K            0x6B    // k
+#define ASCII_SMALL_LETTER_L            0x6C    // l
+#define ASCII_SMALL_LETTER_M            0x6D    // m
+#define ASCII_SMALL_LETTER_N            0x6E    // n
+#define ASCII_SMALL_LETTER_O            0x6F    // o
+#define ASCII_SMALL_LETTER_P            0x70    // p
+#define ASCII_SMALL_LETTER_Q            0x71    // q
+#define ASCII_SMALL_LETTER_R            0x72    // r
+#define ASCII_SMALL_LETTER_S            0x73    // s
+#define ASCII_SMALL_LETTER_T            0x74    // t
+#define ASCII_SMALL_LETTER_U            0x75    // u
+#define ASCII_SMALL_LETTER_V            0x76    // v
+#define ASCII_SMALL_LETTER_W            0x77    // w
+#define ASCII_SMALL_LETTER_X            0x78    // x
+#define ASCII_SMALL_LETTER_Y            0x79    // y
+#define ASCII_SMALL_LETTER_Z            0x7A    // z
+#define ASCII_LEFT_CURLY_BRACKET        0x7B    // {
+#define ASCII_VERTICAL_BAR              0x7C    // |
+#define ASCII_RIGHT_CURLY_BRACKET       0x7D    // {
+#define ASCII_TILDE                     0x7E    // ~
